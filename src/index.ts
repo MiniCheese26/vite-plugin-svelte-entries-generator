8bdb31e3e66eb9870dcd350f95785c91af045ed2
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-03-31 17:59:06 +0100

    make the library much generic

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import {Plugin} from 'vite';
 import pathFs from 'path';
 import fs from 'fs/promises';
-import {glob} from 'glob';
+
+export type EntriesTransformFunction = (apiPath: string, repoRoot: string) => Promise<string | string[]> | string | string[]
 
 interface Path {
-    contentPath: string,
-    entriesBasePath: string,
-    transform?: (filePath: string, entriesBasePath: string) => Promise<string> | string,
+    apiPath: string,
+    transform: EntriesTransformFunction,
 }
 
 export interface PluginOptions {
@@ -16,11 +16,7 @@ export interface PluginOptions {
 const PLUGIN_NAME = 'vite-plugin-svelte-entries-generator';
 const SVELTE_CONFIG_FILE = 'svelte.config.js';
 
-const baseTransformPaths = (file: string, entriesBasePath: string) => {
-    const extension = pathFs.extname(file);
-    const fileName = pathFs.basename(file, extension);
-    return `"${entriesBasePath}${fileName}"`;
-};
+let hasRun = false;
 
 const plugin = ({paths}: PluginOptions): Plugin => {
     return {
@@ -28,27 +24,21 @@ const plugin = ({paths}: PluginOptions): Plugin => {
         apply: 'build',
         enforce: 'pre',
         config: async (config) => {
+            if (hasRun) {
+                return;
+            }
+
+            hasRun = true;
+
             if (!paths) {
                 return;
             }
 
             const root = config.root ?? process.cwd();
 
-            const files = ['"*"', ...(await Promise.all(paths.map(async (path) => {
-                const fullPath = pathFs.resolve(root, path.contentPath);
-
-                const transformPaths = path.transform ?? baseTransformPaths;
-
-                let fullPaths: string[];
-
-                try {
-                    fullPaths = await glob(fullPath);
-                } catch (e) {
-                    console.error(`Failed to read ${fullPath}`);
-                    return [];
-                }
-
-                return await Promise.all(fullPaths.map(async (file) => await transformPaths(file, path.entriesBasePath)));
+            const entries = ['"*"', ...(await Promise.all(paths.map(async (path) => {
+                console.info(`[vite-plugin-svelte-entries-generator] - Running transformer for ${path.apiPath}`)
+                return path.transform(path.apiPath, root);
             }))).flat()];
 
             const svelteConfigPath = pathFs.resolve(root, SVELTE_CONFIG_FILE);
@@ -61,7 +51,7 @@ const plugin = ({paths}: PluginOptions): Plugin => {
             }
 
             const cleanedSvelteConfig = svelteConfig.replace(/entries:\s\[.+],/gmi, '');
-            const modifiedSvelteConfig = cleanedSvelteConfig.replace(/prerender:\s*\{/gmi, `$&entries: [${files.join(',')}],`);
+            const modifiedSvelteConfig = cleanedSvelteConfig.replace(/prerender:\s*\{/gmi, `$&entries: [${entries.join(',')}],`);
 
             try {
                 await fs.writeFile(svelteConfigPath, modifiedSvelteConfig);
